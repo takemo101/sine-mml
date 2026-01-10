8de2c5dc30ce82741ec64830d26a08d12dc50ef9
takemo101 <takemo101@gmail.com>
2026-01-10 21:20:32 +0900

    Rewrite waveform.rs to fix AudioUnit type and clean up.

--- src/audio/waveform.rs
+++ src/audio/waveform.rs
@@ -11,7 +11,9 @@ pub fn midi_to_frequency(note: u8) -> f32 {
     440.0 * 2.0f32.powf((note as f32 - 69.0) / 12.0)
 }
 
-pub fn create_node(waveform: WaveformType, freq: f32) -> Box<dyn AudioUnit64> {
+// fundsp 0.18: AudioUnit is the trait. AudioUnit64 is a typedef for Box<dyn AudioUnit>.
+// If AudioUnit64 is not found, we use Box<dyn AudioUnit>.
+pub fn create_node(waveform: WaveformType, freq: f32) -> Box<dyn AudioUnit> {
     let freq = freq as f64;
     match waveform {
         WaveformType::Sine => Box::new(sine_hz(freq)),
