0fb005e2d486f2ed44c140e4d414fc7369d37e4f
takemo101 <takemo101@gmail.com>
2026-01-12 17:09:43 +0900

    Add allow(clippy::too_many_lines) to fmt function

--- src/mml/error.rs
+++ src/mml/error.rs
@@ -65,6 +65,7 @@ pub enum ParseError {
 }
 
 impl std::fmt::Display for ParseError {
+    #[allow(clippy::too_many_lines)]
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::UnexpectedToken {
