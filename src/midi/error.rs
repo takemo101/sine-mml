a68809364444c7d7cfe51e12104b0dc532507afd
takemo101 <takemo101@gmail.com>
2026-01-14 23:38:36 +0900

    Create placeholder error module (full implementation in #149)

---
+++ src/midi/error.rs
@@ -0,0 +1,8 @@
+//! MIDI error types
+//!
+//! This module defines error types for MIDI operations.
+
+/// Placeholder for future MIDI error types.
+/// Actual error types will be implemented in Issue #149.
+#[derive(Debug)]
+pub struct MidiErrorPlaceholder;
